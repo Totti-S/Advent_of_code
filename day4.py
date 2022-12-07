1c50c9807805aa8242bf989ed4d72f4296e217b1
Totti <totti.sillanpaa@gmail.com>
2022-12-07 19:02:37 +0200

    Added comments

--- day4.py
+++ day4.py
@@ -15,6 +15,16 @@ def main():
         elf_one_tasks = split_tasks(pair_list[0])
         elf_two_tasks = split_tasks(pair_list[1])
         
+        # Solution uses few key observation:
+        #   Obvious: If fully contained then it's also overlapping
+
+        #   1. If the first tasks or the last tasks are the same then one elfs tasks is fully contained within other
+        #   2. If first elfs task is smaller the second elfs task and same for to second tasks, but bigger then second elf is fully contained
+        #       i. Chance the order of the elfs and you get rest of the answers
+        #   3. Overlaping needs two more results: (elf1 : E1  ; elf2 : e1) -> First number elf and second task
+        #       i.  E1 e1 E2 e2
+        #       ii. e1 E1 e2 E2
+
         if elf_one_tasks[0] == elf_two_tasks[0] or elf_one_tasks[1] == elf_two_tasks[1]:
             overlap_sum += 1
             fully_contained += 1
