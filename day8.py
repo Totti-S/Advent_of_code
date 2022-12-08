e6af2e4efbb8e03dc65940a6b539f394d447b1a9
Totti <totti.sillanpaa@gmail.com>
2022-12-08 16:22:03 +0200

    Made some improvments but overall solution is same struct

--- day8.py
+++ day8.py
@@ -8,73 +8,68 @@ def main():
 
     seen_trees = []
 
+    def find_trees(line, idx, vertical, backwards):
+        length = len(line)
+        line = reversed(line) if backwards else line
+
+        largest_tree = -1
+        for x, tree in enumerate(line):
+            tree = int(tree)
+            if largest_tree < int(tree):
+                x = length - (x+1) if backwards else x
+                largest_tree = tree
+                seen_trees.append((idx,x) if vertical else (x,idx))
+
+    # Go through all the data rows
     for i,row in enumerate(data):
         row = row.strip()
-        largest_tree = -1
-        for j,tree in enumerate(row):
-            tree = int(tree)
-            if largest_tree < int(tree):
-                largest_tree = tree
-                seen_trees.append((i,j))
 
-        largest_tree = -1
-        for j,tree in enumerate(reversed(row)):
-            j = r_size - j -1
-            tree = int(tree)
-            if largest_tree < int(tree):
-                largest_tree = tree
-                seen_trees.append((i,j))
+        find_trees(row, i, False, False)
+        find_trees(row, i, False, True)
 
+    # Go through all the data column
     for j in range(r_size):
         column = ""
         for i in range(c_size):
             column += data[i][j]
         
-        largest_tree = -1
-        for i,tree in enumerate(column):
-            tree = int(tree)
-            if largest_tree < int(tree):
-                largest_tree = tree
-                seen_trees.append((i,j))
+        find_trees(column,j, True, False)
+        find_trees(column,j, True, True)
 
-        largest_tree = -1
-        for i,tree in enumerate(reversed(column)):
-            i = c_size - i -1
-            tree = int(tree)
-            if largest_tree < int(tree):
-                largest_tree = tree
-                seen_trees.append((i,j))
-
-    
-
+    # Remove repeated 
     unique_trees = list(set(seen_trees))
     print(len(unique_trees))
-    print(len(seen_trees))
 
-    def tree_distance_counter(marker_tree, check_range, ref_idx, row=True):
+    # Part 2
+
+    # We use VERY generalized function to loop from given starting point
+    def tree_distance_counter(row, column, direction):
+        backwards = direction in ['N', 'W']
+        vertical = direction in ['N', 'S']
+        fixed_idx = column if vertical else row
+        marker_tree = int(data[row][column])
+        
+        s = (row if vertical else column) + (-1 if backwards else 1)
+        e = -1 if backwards else c_size if vertical else r_size
+        inc = -1 if backwards else 1
+        
         distance = 0
-        for x in check_range:
+        for x in range(s,e,inc):
             distance += 1
-            target_tree = int(data[ref_idx][x]) if row else int(data[x][ref_idx])
-            if target_tree >= marker_tree:
+            target_tree = data[x][fixed_idx] if vertical else data[fixed_idx][x]
+            if int(target_tree) >= marker_tree: 
                 break
         return distance
 
+    # Actual solution to part 2
     scenic_score = 0
     for row in range(c_size):
         for column in range(r_size):
-            marker_tree = int(data[row][column])
-
-            range1 = reversed(range(column))
-            range2 = range(column+1,r_size)
-            range3 = reversed(range(row))
-            range4 = range(row+1,c_size)
-
-            # Labeling idea from Eetu Knutas
-            dist1 = tree_distance_counter(marker_tree, range1, row, True) # West
-            dist2 = tree_distance_counter(marker_tree, range2, row, True) # East
-            dist3 = tree_distance_counter(marker_tree, range3, column, False) # North
-            dist4 = tree_distance_counter(marker_tree, range4, column, False) # South
+            # Labeling idea from Eetu Knutas turned into better generalized function
+            dist1 = tree_distance_counter(row, column, "N") # North
+            dist2 = tree_distance_counter(row, column, "E") # East
+            dist3 = tree_distance_counter(row, column, "S") # South
+            dist4 = tree_distance_counter(row, column, "W") # West
 
             tree_score = dist1*dist2*dist3*dist4
             scenic_score = tree_score if tree_score > scenic_score else scenic_score
