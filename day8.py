d8fb2cf1c6cadbb331259d9964c6bed6c5bc2069
Totti <totti.sillanpaa@gmail.com>
2022-12-08 14:45:07 +0200

    Made generalized function to second part

--- day8.py
+++ day8.py
@@ -1,4 +1,3 @@
-from math import prod 
 def main():
 
     with open("day8_data.txt", "r") as f:
@@ -7,7 +6,6 @@ def main():
     c_size = len(data)
     r_size = len(data[0].strip())
 
-    visable_trees = 0
     seen_trees = []
 
     for i,row in enumerate(data):
@@ -53,55 +51,35 @@ def main():
     print(len(unique_trees))
     print(len(seen_trees))
 
+    def tree_distance_counter(data, marker_tree, check_range, ref_idx, row=True):
+        distance = 0
+        for x in check_range:
+            distance += 1
+            target_tree = int(data[ref_idx][x]) if row else int(data[x][ref_idx])
+            if target_tree >= marker_tree:
+                break
+        return distance
+
     scenic_score = 0
     for row in range(c_size):
         for column in range(r_size):
             marker_tree = int(data[row][column])
-            tree_distances = []
 
-            distance = 0
-            for i in reversed(range(0,row)):
-                distance += 1
-                target_tree = int(data[i][column])
-                if target_tree >= marker_tree:
-                    break
-            tree_distances.append(distance)
-            
-            distance = 0
-            for i in range(row+1,c_size):
-                distance += 1
-                target_tree = int(data[i][column])
-                if target_tree >= marker_tree:
-                    break
-            tree_distances.append(distance)
+            range1 = reversed(range(column))
+            range2 = range(column+1,r_size)
+            range3 = reversed(range(row))
+            range4 = range(row+1,c_size)
 
-            distance = 0
-            for j in reversed(range(0,column)):
-                distance += 1
-                target_tree = int(data[row][j])
-                if target_tree >= marker_tree:
-                    break
-            tree_distances.append(distance)
+            # Labeling idea from Eetu Knutas
+            dist1 = tree_distance_counter(data, marker_tree, range1, row, True) # West
+            dist2 = tree_distance_counter(data, marker_tree, range2, row, True) # East
+            dist3 = tree_distance_counter(data, marker_tree, range3, column, False) # North
+            dist4 = tree_distance_counter(data, marker_tree, range4, column, False) # South
 
-            
-            distance = 0
-            for j in range(column+1,r_size):
-                distance += 1
-                target_tree = int(data[row][j])
-                if target_tree >= marker_tree:
-                    break
-            tree_distances.append(distance)
-
-            
-            tree_score = prod(tree_distances)
+            tree_score = dist1*dist2*dist3*dist4
             scenic_score = tree_score if tree_score > scenic_score else scenic_score
     
     print(scenic_score)
-            
-            
 
-            
-
-    
 if __name__ == "__main__":
     main()
