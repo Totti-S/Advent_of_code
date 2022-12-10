d38069d40ee8cce0c66eb1bb3476ddcfe81b72ac
Totti <totti.sillanpaa@gmail.com>
2022-12-10 08:47:04 +0200

    Removed reduntant variable, cleaned the code

--- day10.py
+++ day10.py
@@ -11,29 +11,18 @@ def main():
             x_values.append(x_values[-1] + int(command[1]))
 
     signal_sum = 0
-
     for idx in range(20,221,40):
         signal_sum += idx*x_values[idx-1]
     
-    print(signal_sum)
+    print(signal_sum)   # Part1 solution
 
-    screen_output = ""
+    screen_output = [[""] * 6]
     for idx, x in enumerate(x_values):
-        col_index = idx % 40 
-        if col_index >= x-1 and col_index <= x+1:
-            screen_output += "#" 
-        else:
-            screen_output += "."
-    
-    screen_lines = []
-    for i in range(0,len(screen_output),40):
-        screen_lines.append(screen_output[i:i+40])
+        row, col = divmod(idx,40) 
+        screen_output[row] += "#" if x-1 <= col <= x+1 else "."
 
-    for line in screen_lines:
+    for line in screen_output:  # Part2 solution
         print(line)
-
-
-
 
 if __name__ == "__main__":
     main()
