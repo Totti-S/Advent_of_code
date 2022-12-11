f9f33af53ea2977b11dc077353e3c6fdbb6e23db
Totti <totti.sillanpaa@gmail.com>
2022-12-11 17:28:53 +0200

    Uses eval instead of whatever we did before

--- day11.py
+++ day11.py
@@ -12,24 +12,11 @@ def main():
         dividers = [2,3,5,7,11,13,17,19,23]
         def __init__(self, number):
             self.reminders = [number%num for num in Item.dividers]
-        def plus(self, number):
-            self.reminders = [(num + number) % divider for num,divider in zip(self.reminders, Item.dividers)]
-        def times(self, number):
-            self.reminders = [(num * number) % divider for num,divider in zip(self.reminders, Item.dividers)]
-        def times_itself(self):
-            self.reminders = [(num * num) % divider for num,divider in zip(self.reminders, Item.dividers)]
 
     class Monkey():
-        def __init__(self,items, operation, operation_num, div_test, true_idx, false_idx):
+        def __init__(self, items, operation, div_test, true_idx, false_idx):
             self.items = items
-
-            if operation_num == "old":
-                self.operation = "**"
-                self.operation_num = 2
-            else:
-                self.operation = operation
-                self.operation_num = int(operation_num)
-            
+            self.operation = eval('lambda old: ' + operation) # Credit for reminding me about eval function: Brian Chen solution
             self.div_test = int(div_test)
             self.if_true = int(true_idx)
             self.if_false = int(false_idx)
@@ -38,19 +25,13 @@ def main():
         def do(self):
             if self.items:
                 for item in self.items:
-                    if self.operation == "+":
-                        item.plus(self.operation_num)
-                    elif self.operation == "*":
-                        item.times(self.operation_num)
-                    else:
-                        item.times_itself()
+                    item.reminders = [self.operation(num) % divider for num,divider in zip(item.reminders, Item.dividers)]
                     rem_idx = Item.dividers.index(self.div_test)
                     idx = self.if_false if item.reminders[rem_idx] % self.div_test else self.if_true
                     monkeys[idx].items.append(item)
 
                 self.looked_items += len(self.items)
                 self.items = []
-
 
     monkeys_data = data.split("\n\n")
     monkeys = []
@@ -64,16 +45,18 @@ def main():
                 tmp = info_field.split(":")[1].split(",")
                 items = [Item(int(i)) for i in tmp]
                 init_info.append(items)
-            elif i > 1:
+            elif i == 2:
+                operation_string = info_field.split("=")[-1]
+                init_info.append(operation_string.strip())
+            elif i > 2:
                 list_things = info_field.split(" ")
-                if i == 2:
-                    init_info.append(list_things[-2])
                 init_info.append(list_things[-1])
 
         monkeys.append(Monkey(*init_info))
 
     num_rounds = 10_000
     for r in range(0,num_rounds):
+        print(r)
         for monkey in monkeys:
             monkey.do()
 
