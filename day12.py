35405f277d1e65d874b0adcf40075d05bd81cb68
Totti <totti.sillanpaa@gmail.com>
2022-12-12 22:55:55 +0200

    Uses numpy array for better index access, stripped the algorithm to only hold neccessary parts for completion, also both parts now in same run

--- day12.py
+++ day12.py
@@ -2,70 +2,36 @@ import numpy as np
 def main():
     with open("data/day12_data.txt", "r") as f:
         data = f.readlines()
+    
+    # Let's make numpy matrix from the string data
+    data = np.array([list(line.strip()) for line in data])
 
-    a_ord = ord('a')
-    data_matrix = []
-    start_pos,end_pos = 0,0
-    for i,line in enumerate(data):
-        line = line.strip()
-        numbers = []
-        for j,char in enumerate(line):
-            if char == "S":
-                start_pos = (i,j)
-                numbers.append(0)
-            elif char == "E":
-                end_pos = (i,j)
-                numbers.append(ord('z')-ord('a'))
-            else:
-                numbers.append(ord(char)-ord('a'))
-        data_matrix.append(numbers)
+    # First find the start and end points
+    start_pos = np.where(data == 'S')
+    end_pos = np.where(data == "E")
 
-    # calculates distabce to endpoint
-    def h(node, endNode):
-        return np.sqrt(np.square(abs(node[0]-endNode[0])) + np.square(abs(node[1]-endNode[1])))
+    # Replace with 'correct letters'
+    data[start_pos] = "a"
+    data[end_pos] = "z"
 
-
-    def distance(start_node, end_node, matrix):
-        a_value = matrix[start_node[0]][start_node[1]]
-        b_value = matrix[end_node[0]][end_node[1]]
-        if a_value - b_value <= 1:
-            return 1
-        else:
-            return np.inf
-
-    def reconstruct_path(came_from, current):
-        total_path = [current]
-        while current[0] != -1 or current[1] != -1:
-            current = came_from[current[0]][current[1]]
-            total_path.append(current)
-        return total_path
-
-    def A_star(s,e, matrix):
+    # Search algorithm was orginally wikipedia A* psedo-code implementation
+    # After stripping this down, this has no heuristic function, so basically
+    # some kind of uniform-search algorithm
+    def A_star(s,e, matrix, part):
         open_set = [s]
         row_size, col_size = np.shape(matrix)
 
-        value = np.empty((), dtype=object)
-        value[()] = (-1, -1)
-        came_from = np.full(np.shape(matrix),value, dtype=object)
-        
+        # This keeps track of distance to each node
         g_scores = np.full(np.shape(matrix), np.inf)
-        g_scores[s[0]][s[1]] = 0
-
-        f_scores = np.full(np.shape(matrix), np.inf)
-        #f_scores[s[0]][s[1]] = h(s,e)
+        g_scores[s] = 0
 
         while open_set:
-            lowest_f = np.inf
-            current = (0,0)
-            for node in open_set:
-                if f_scores[node[0]][node[1]] < lowest_f:
-                    lowest_f = f_scores[node[0]][node[1]]
-                    current = node
-
             current = open_set[0]
 
-            if matrix[current[0]][current[1]] == 0:
-                return reconstruct_path(came_from, current)
+            if part == 1 and current == e:
+                return g_scores[current]
+            elif part == 2 and matrix[current][0] == e:
+                return g_scores[current]
             
             open_set.remove(current)
             neighbors = []
@@ -79,26 +45,24 @@ def main():
                 neighbors.append((current[0]-1, current[1]))
 
             for neighbor in neighbors:
-                tentative_g_score = g_scores[current[0]][current[1]] + distance(current, neighbor,matrix)
-                if tentative_g_score < g_scores[neighbor[0]][neighbor[1]]:
-                    came_from[neighbor[0]][neighbor[1]] = current
-                    g_scores[neighbor[0]][neighbor[1]] = tentative_g_score
-                    #f_scores[neighbor[0]][neighbor[1]] = tentative_g_score + h(neighbor, e)
+                tentative_g_score = g_scores[current]
+                if part == 1:
+                    tentative_g_score += 1 if ord(matrix[neighbor][0]) - ord(matrix[current][0]) <= 1 else np.inf
+                else:
+                    tentative_g_score += 1 if ord(matrix[current][0]) - ord(matrix[neighbor][0]) <= 1 else np.inf
+                
+                if tentative_g_score < g_scores[neighbor]:
+                    g_scores[neighbor] = tentative_g_score
                     if neighbor not in open_set:
                         open_set.append(neighbor)
         
         print("FAILED!")
         return
 
-
-    # path = A_star(start_pos, end_pos, data_matrix)
-    # print(path)
-    # print(len(path)-2)
-
-    path2 = A_star(end_pos,(0,1),data_matrix)
-    print(path2)
-    print(len(path2)-2)
-
+    silver = A_star(start_pos, end_pos, data, part=1)
+    gold = A_star(end_pos, "a", data, part=2)
+    print(f"Silver: {int(silver)}")
+    print(f"Gold: {int(gold)}")
 
 if __name__ == "__main__":
     main()
