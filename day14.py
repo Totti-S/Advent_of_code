fc84dd4f1a88334a0eb1e31b867fe183e83c5fad
Totti <totti.sillanpaa@gmail.com>
2022-12-14 18:33:48 +0200

    Cleaning part2

--- day14.py
+++ day14.py
@@ -1,56 +1,52 @@
 import numpy as np
 def main():
-    with open("data/day14_data.txt", "r") as f:
+    with open("data/day14_test_data.txt", "r") as f:
         data = f.readlines()
 
     lists = [row.strip().split(" -> ") for row in data]
+    cols = [int(num.split(",")[0]) for row in lists for num in row]
+    rows = [int(num.split(",")[1]) for row in lists for num in row]
     
-    cols = [[int(num.split(",")[0]) for num in row] for row in lists]
-    rows = [[int(num.split(",")[1]) for num in row] for row in lists]
+    start_col = min(cols)
+    col_len = max(cols) - start_col + 1
+    
+    grid = np.zeros((max(rows)+1, col_len))
+    
+    # Fill empty grid with 1 if there is wall
+    row_idx, col_idx = -1, -1
+    for row, col in zip(rows, cols): 
+        if col_idx == col:
+            inc = 1 if row - row_idx > 0 else -1
+            s_col = col_idx - start_col
+            grid[row_idx:row+inc:inc, s_col] = 1
+        elif row_idx == row:
+            inc = 1 if col - col_idx > 0 else -1
+            s_col = col_idx - start_col
+            tmp = col - start_col
+            e_col = (tmp+inc if tmp else None)  # Hack for including 0 index backwards slicing
+            grid[row_idx, s_col:e_col:inc] = 1
+        
+        row_idx, col_idx = row, col
 
-    col_nums = [int(num.split(",")[0]) for row in lists for num in row]
-    row_nums = [int(num.split(",")[1]) for row in lists for num in row]
-    
-    col_len = max(col_nums) - min(col_nums) + 1
-    
-    grid = np.zeros((max(row_nums)+1, col_len)) # Add padding for sand go there
-    
-    start_col = min(col_nums)
-
-    for row, col in zip(rows, cols):
-        row_idx, col_idx = -1, -1
-        for r, c in zip(row, col):
-            if row_idx == -1:
-                row_idx, col_idx = r,c
-                continue
-            if col_idx == c:
-                inc = 1 if r - row_idx > 0 else -1
-                idx_c = col_idx - start_col 
-                grid[row_idx:r+inc:inc, idx_c] = 1
-            else:
-                inc = 1 if c - col_idx > 0 else -1
-                s_col = col_idx - start_col
-                e_col = c - start_col
-                grid[row_idx, s_col:(e_col+inc if e_col else None):inc] = 1    # Hack for including 0 index backwards slicing
-
-            row_idx, col_idx = r,c
-    
+    # Two rows below: one empty and one floor
     grid = np.vstack((grid, np.zeros(col_len)))
     grid = np.vstack((grid, np.ones(col_len)))
     
-    col = 500 - start_col
-    row = 0
-    row_size, col_size = np.shape(grid)
-    row_size -= 1
-    col_size -= 1
+    row_size = np.shape(grid)[0] - 1
+    col_size = np.shape(grid)[1] - 1
+
     silver, silver_flag = 0, False
     sand_units = 0
+    source_col = 500 - start_col
+    col = source_col
+    row = 0
     while True:
         if not silver_flag and row == row_size-1:
             silver = sand_units
             silver_flag = True
         
-        if col_size == col+1:
+        # add columns to grid if close border 
+        if col+1 == col_size:
             grid = np.hstack((grid, np.zeros((np.shape(grid)[0],1))))
             grid[-1, -1] = 1
             col_size += 1
@@ -58,7 +54,7 @@ def main():
             grid = np.hstack((np.zeros((np.shape(grid)[0],1)), grid))
             grid[row_size, 0] = 1
             col_size += 1
-            start_col -= 1
+            source_col += 1
 
         if not grid[row+1, col]:
             row += 1
@@ -71,9 +67,9 @@ def main():
         else:
             grid[row, col] = 1
             sand_units += 1
-            if row == 0 and col == 500 - start_col:
+            if row == 0 and col == source_col:
                 break
-            col = 500 - start_col
+            col = source_col
             row = 0
 
     print(f'Silver: {silver}')
