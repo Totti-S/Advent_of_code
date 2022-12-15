ac93c4c832e6bf25c6ef8f9da7ada5381680a766
Totti <totti.sillanpaa@gmail.com>
2022-12-15 15:19:09 +0200

    Comments and cleaning

--- day15.py
+++ day15.py
@@ -16,10 +16,9 @@ def main():
         beacons.append((x_b,y_b))
 
     silver = -1
-    gold = -1
 
-    line_of_intress = 2_000_000
-    not_available_line_points = set()
+    line_of_intress = 2_000_000                                         
+    not_available_line_points = set()                                   
     for sensor, beacon in zip(sensors, beacons):
         s_x, s_y = sensor
         b_x, b_y = beacon
@@ -30,7 +29,8 @@ def main():
         if dist_to_line < dist_to_beacon:
             leftover_dist = dist_to_beacon - dist_to_line
             not_available_line_points.update([x for x in range(s_x-leftover_dist, s_x+leftover_dist+1)])
-        
+    
+    # Remove Sensors and Beacons from data
     for beacon in beacons:
         x,y = beacon
         if y == line_of_intress and x in not_available_line_points:
@@ -43,39 +43,42 @@ def main():
             
     silver = len(not_available_line_points)
 
-    # Part 2
+    ############# Part 2
 
+    # Small optimization: only once calculate sensor-beacon distances 
     distances_to_beacon = []
     for sensor, beacon in zip(sensors, beacons):
         s_x, s_y = sensor
         b_x, b_y = beacon
         distances_to_beacon.append(abs(s_y - b_y) + abs(s_x - b_x))
 
-    
     gold = -1
     upper_bound = 2*line_of_intress
+
+    # Solution: Go each point in the grid through and check if the point is
+    # contained in one of the sensor-beacon radius. If not, we have found the
+    # the missing beacon
     for j in range(0,upper_bound+1):
-        
         i = 0
         while i <= upper_bound:
             for sensor, distance in zip(sensors, distances_to_beacon):
                 s_x, s_y = sensor
                 dist_to_point = abs(s_y - j) + abs(s_x - i)
                 if dist_to_point <= distance:
-                    i += distance - dist_to_point
-                    break
-            else:
+                    i += distance - dist_to_point   # <- This is important for performance:
+                    break                           # We can extrapolate all the points that can't
+            else:                                   # contain the beacon, so we jump ahead
                 gold = i*4_000_000 + j
 
             if gold != -1:
                 break
             i += 1
+        
         if gold != -1:
             break
 
     print(f'Silver: {silver}')
     print(f'Gold: {gold}')
-
-        
+     
 if __name__ == "__main__":
     main()
