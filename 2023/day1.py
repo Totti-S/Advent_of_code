91f6bf07f97ea3c81e61eb13c35bf0d08b0c6efb
Totti-S <70943416+Totti-S@users.noreply.github.com>
2023-11-30 23:39:00 +0200

    Added 2023 day1 tmp file

---
+++ 2023/day1.py
@@ -0,0 +1,10 @@
+def main():
+    with open("data/day01_data.txt", "r") as f:
+        data = f.readlines()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ utilities/day00.py
@@ -0,0 +1,11 @@
+def main():
+    with open("data/day01_data.txt", "r") as f:
+        data = f.readlines()
+
+    # Conversion str -> int
+    tmp_to_int = [(map(int, tmp)) for tmp in tmp_list]
+
+
+
+if __name__ == "__main__":
+    main()
