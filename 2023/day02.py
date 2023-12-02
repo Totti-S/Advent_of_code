4cd2aa9a7b9b52afa30066a9bab71c7ba134aa32
Totti Sillanpää <totti.sillanpaa@gmail.com>
2023-12-02 07:43:08 +0200

    Silver and Gold day2

--- 2023/day02.py
+++ 2023/day02.py
@@ -2,9 +2,47 @@ from utilities.get_data import get_data
 def main(mode='silver', data_type=''):
     data = get_data(__file__, data_type)
 
-    # Conversion str -> int
-    tmp_to_int = [(map(int, tmp)) for tmp in tmp_to_int]
-
+    cubes = ['blue', 'red', 'green']
+    round_results = []
+    for line in data:
+        line = line.split(":")
+        tmp = line[1]
+        grabs = tmp.split(";")
+        round = []
+        for one_grab in grabs:
+            result_cubes = one_grab.split(',')
+            res = [0 ,0 ,0]
+            for c in result_cubes:
+                c = c.strip().split(" ")
+                res[cubes.index(c[1])] = int(c[0])
+            round.append(res)
+        round_results.append(round)
+    # Silver solution
+    max_cubes = [14,12,13]
+    total = 0
+    for i, round in enumerate(round_results,1):
+        could_happen = True
+        for result in round:
+            for j in range(0,3):
+                if result[j] > max_cubes[j]:
+                    could_happen = False
+        if could_happen:
+            total += i
+    print(total)
+    
+    # Gold solution
+    total_2nd = 0
+    for round in round_results:
+        min_cubes = [-1,-1,-1]
+        power = 1
+        for result in round:
+            for j in range(0,3):
+                if min_cubes[j] == -1 or result[j] > min_cubes[j]:
+                    min_cubes[j] = result[j]
+        for x in min_cubes:
+            power *= x
+        total_2nd += power
+    print(total_2nd)
 
 
 if __name__ == "__main__":
