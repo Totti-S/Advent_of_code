bd4c459eea0398aa03537b1aa854b1c7e1b4ed7b
Totti Sillanpää <totti.sillanpaa@gmail.com>
2023-12-02 08:11:18 +0200

    Small chances

--- 2023/day02.py
+++ 2023/day02.py
@@ -5,9 +5,8 @@ def main(mode='silver', data_type=''):
     cubes = ['blue', 'red', 'green']
     round_results = []
     for line in data:
-        line = line.split(":")
-        tmp = line[1]
-        grabs = tmp.split(";")
+        # Ignore 'Game x:' portion and split game to grabs of cubes
+        grabs = line.split(":")[1].split(';')
         round = []
         for one_grab in grabs:
             result_cubes = one_grab.split(',')
@@ -17,28 +16,32 @@ def main(mode='silver', data_type=''):
                 res[cubes.index(c[1])] = int(c[0])
             round.append(res)
         round_results.append(round)
+
     # Silver solution
     max_cubes = [14,12,13]
     total = 0
     for i, round in enumerate(round_results,1):
         could_happen = True
         for result in round:
-            for j in range(0,3):
-                if result[j] > max_cubes[j]:
+            for r, m in zip(result, max_cubes):
+                if r > m:
                     could_happen = False
         if could_happen:
             total += i
     print(total)
-    
+
     # Gold solution
     total_2nd = 0
+    min_cubes = []
     for round in round_results:
-        min_cubes = [-1,-1,-1]
         power = 1
-        for result in round:
-            for j in range(0,3):
-                if min_cubes[j] == -1 or result[j] > min_cubes[j]:
-                    min_cubes[j] = result[j]
+        for j, result in enumerate(round):
+            if not j: # zero index resets min_cubes
+                min_cubes = result
+                continue
+            for k in range(0,3):
+                if result[k] > min_cubes[k]:
+                    min_cubes[k] = result[k]
         for x in min_cubes:
             power *= x
         total_2nd += power
