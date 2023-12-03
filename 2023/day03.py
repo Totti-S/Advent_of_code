8ec81f33007da903e70e40889303f1ae2f3a3d66
Totti Sillanpää <totti.sillanpaa@gmail.com>
2023-12-03 17:16:45 +0200

    Looping -> row by row (instead of column)

--- 2023/day03.py
+++ 2023/day03.py
@@ -10,31 +10,16 @@ def main(mode='silver', data_type=''):
     #
     # Gold:     If the symbol we find is * then record the number in dictionary using 
     #           row and col as key. Accept only list that has two entries
+    # After completion: Instead of column by column -> row by row
+    # Inspiration credit: https://github.com/PauliAnt/advent_of_code/blob/main/day3/day3.ipynb
 
-    gears = defaultdict(list)
-    def check_for_symbol(r, c, num):
-        if data[r][c] == "*":
-            gears[(r,c)].append(num)
-        return data[r][c] != '.'
-    
+    gears = defaultdict(list)  
     total, gear_sum = 0, 0
     for row, line in enumerate(data):
         number = None
-        def scan(column, middle=False):
-            if row != 0:
-                if check_for_symbol(row-1, column, number): 
-                    return True
-            if not middle:
-                if check_for_symbol(row, column, number): 
-                    return True
-            if row + 1 != len(data):
-                if check_for_symbol(row+1, column, number): 
-                    return True
-            return False
-
         start, end = None, None
         for col, char in enumerate(line):
-            if start is not None and end is not None:
+            if start is not None and end is not None: # Reset after number
                 start, end = None, None
             if type(char) == int:
                 if start is None:
@@ -43,31 +28,30 @@ def main(mode='silver', data_type=''):
                     continue
                 end = col
                 number = int("".join([str(line[x]) for x in range(start,end+1)]))
-                
-                # check start
-                if start != 0 and scan(start-1): 
-                    total += number
-                    continue
 
-                # check middle
-                middle_check = False
-                for k in range(start, end+1):
-                    middle_check = scan(k, True)
-                    if middle_check: break
-                if middle_check: 
-                    total += number
-                    continue
+                middle_range = [] # Middle row -> check start and end (if not at the border) 
+                if start - 1 >= 0:
+                    middle_range.append(start-1)
+                if end + 1 < len(line):
+                    middle_range.append(end+1)
 
-                # check end
-                if end+1 != len(line) and scan(end+1): 
-                    total += number
+                symbol_found = False
+                for row_i in range(max(0,row-1), min(len(data), row+2)):
+                    interate_over = middle_range if row_i == row else range(max(0, start-1), min(len(line), end+2))
+                    for col_i in interate_over:
+                        if data[row_i][col_i] == "*":
+                            gears[(row_i,col_i)].append(number)
+                        if data[row_i][col_i] != '.':
+                            total += number
+                            symbol_found = True
+                            break
+                    if symbol_found: break
 
     print(total) # Silver
     
-    from math import prod
     for gear in gears.values():
         if len(gear) == 2:
-            gear_sum += prod(gear)
+            gear_sum += gear[0] * gear[1]
     print(gear_sum) # Gold
 
 if __name__ == "__main__":
