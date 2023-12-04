4f371d8389a1ce1a0d080cabc00cc71b45c86e37
Totti Sillanpää <totti.sillanpaa@gmail.com>
2023-12-04 08:19:54 +0200

    Code golfing result

--- 2023/day04.py
+++ 2023/day04.py
@@ -1,38 +1,24 @@
 from utilities.get_data import get_data
-from collections import defaultdict
 def main(mode='silver', data_type=''):
     data = get_data(__file__, data_type)
-    data = [x.split(": ")[1] for x in data]
 
-    copies = {x : 1 for x in range(0, len(data))}
+    # Todays puzzle was the most straight forward so far.
+    # Code comments itself
+    data = [x.split(": ")[1] for x in data] # Filter out 'Card x: '
+
+    copies = [1 for _ in range(0, len(data))]
     total = 0
     for i, line in enumerate(data):
-        winning, your_numbers = line.split("|")
-        winning = winning.split()
-        winning = [int(tmp) for tmp in winning]
-
-        your_numbers = your_numbers.split()
-        your_numbers = [int(tmp) for tmp in your_numbers]
-        if mode == 'silver':
-            card_total = 0
-            for num in your_numbers:
-                if num in winning:
-                    card_total += 1
-            if card_total != 0:
-                total += 2** (card_total-1)
-        else:
-            card_total = 0
-            for num in your_numbers:
-                if num in winning:
-                    card_total += 1
+        winning, your_numbers = [[int(num) for num in str_nums.split()] for str_nums in line.split("|")]
+        card_total = sum([(num in winning) for num in your_numbers])
+        if card_total > 0:
+            total += 2** (card_total-1)
             for x in range(1, card_total+1):
                 copies[i+x] += copies[i]
-    
-    total = sum(copies.values())
-            
-    print(f'total {total}')
+    gold_total = sum(copies)
 
-
+    print(f'Silver: {total}')
+    print(f'Gold: {gold_total}')
 
 if __name__ == "__main__":
     main('gold')
