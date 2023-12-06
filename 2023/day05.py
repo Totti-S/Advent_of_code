55e56ab81117ce4049ac4768e53a4280a6390021
Totti Sillanpää <totti.sillanpaa@gmail.com>
2023-12-06 08:13:26 +0200

    Much better naming scheme and fixed comments

--- 2023/day05.py
+++ 2023/day05.py
@@ -7,92 +7,91 @@ def main(mode='silver', data_type=''):
     data.pop(0)
     data.pop(0)
     print(data)
-    groups = []
+    regions = []
     i = -1
     for line in data:
         if line.endswith('map:'): 
-            groups.append([])
+            regions.append([])
             i +=1
             continue
         if line != '':
-            groups[i].append([int(tmp) for tmp in line.split()])
+            regions[i].append([int(tmp) for tmp in line.split()])
 
     if mode == 'silver':
 
         lowest = None
-        type_index = None
         for seed in seeds:
-            type_index = seed
-            for group in groups:
-                for nums in group:
-                    dest, sour, r = nums
-                    if sour <= type_index <= sour + r:
-                        delta  = type_index - sour
-                        type_index = delta + dest
+            value = seed
+            for region in regions:
+                for sub_region in region:
+                    dest, source, length = sub_region 
+                    if source <= value <= source + length:
+                        value = dest + (value-source)
                         break
-            if lowest is None or type_index < lowest:
-                lowest = type_index
+            if lowest is None or value < lowest:
+                lowest = value
 
     else:
-        ultimate_filter = []
+        seed_to_region = []
         for i in range(0, len(seeds),2):
-            ultimate_filter.append([seeds[i], seeds[i], seeds[i+1]])
+            seed_to_region.append([seeds[i], seeds[i], seeds[i+1]])
         
         g = 0
-        while g < len(groups):
-            group = groups[g]
-            new_ultimate = []
+        while g < len(regions):
+            region = regions[g]
+            new_region = []
+            
             s = 0
-            while s < len(ultimate_filter):
-                dest, source, length = ultimate_filter[s]
+            while s < len(seed_to_region):
+                dest, source, length = seed_to_region[s]
                 a = dest
                 b = dest + length -1
 
                 i = 0
-                while i < len(group):
-                    dest_2, source_2, length_2 = group[i]
+                while i < len(region):
+                    dest_2, source_2, length_2 = region[i]
                     x = source_2
                     y = source_2 + length_2 - 1
                     offset = dest_2 - source_2
-                    if b < x or y < a:
+                    if b < x or y < a:  # Region is outside
                         i += 1
                         continue
                     else:
-                        filter = [0,0,0]
-                        if a <= x and b >= y:
-                            # print('Aikaisemmin täällä')
-                            filter = [dest_2, source + (x-a), length_2]
+                        if a <= x and b >= y: # Region is same, Right side or Left side larger or Both
+                            overlapping = [dest_2, source + (x-a), length_2]
                             if x != a:
-                                ultimate_filter.append([dest, source, x-a])         # left side
+                                seed_to_region.append([dest, source, x-a])
                             if b != y:
-                                ultimate_filter.append([dest + (y-a+1), source + (y-a+1), b-y])    # right side
-                        elif x <= a and b <= y: # Seed inside
-                            filter = [dest + offset, source, length]
+                                seed_to_region.append([dest + (y-a+1), source + (y-a+1), b-y])
+                        
+                        elif a >= x and b <= y: # Region inside or Inside and one side is Same
+                            overlapping = [dest + offset, source, length]
                             if x != a:
-                                group.append([dest_2, source_2, a-x])      # left side
+                                region.append([dest_2, source_2, a-x])
                             if b != y:
-                                group.append([dest_2 + (b-x+1), source_2 + (b-x+1), y-b]) # right side
+                                region.append([dest_2 + (b-x+1), source_2 + (b-x+1), y-b])
                             
-                        elif x < a:  # Crossing left side
-                            filter = [dest + offset, source, (y-a+1)]
-                            group.append([dest_2, source_2, a-x])
-                            ultimate_filter.append([dest + (y-a+1), source +(y-a+1), b-y])
-                        else:   # Seed crossing right side
-                            filter = [dest_2, source + (x-a), (b-x+1)]
-                            group.append([dest_2 + (b-x+1), source_2 + (b-x+1), (y-b)])
-                            ultimate_filter.append([dest, source, (x-a)])
-                        new_ultimate.append(filter)
-                        group.pop(i)
-                        ultimate_filter.pop(s)
+                        elif a > x:  # Regions Right side is inside and left is out
+                            overlapping = [dest + offset, source, (y-a+1)]
+                            region.append([dest_2, source_2, a-x])
+                            seed_to_region.append([dest + (y-a+1), source +(y-a+1), b-y])
+                        
+                        else:  # Regions Left side is inside and right is out
+                            overlapping = [dest_2, source + (x-a), (b-x+1)]
+                            region.append([dest_2 + (b-x+1), source_2 + (b-x+1), (y-b)])
+                            seed_to_region.append([dest, source, (x-a)])
+                        new_region.append(overlapping)
+                        region.pop(i)
+                        seed_to_region.pop(s)
                         s += -1
                     break
                 s += 1
             
-            ultimate_filter += new_ultimate
+            seed_to_region += new_region
             g +=1
 
-        ultimate_filter.sort(key=lambda x: x[0])
-        lowest = ultimate_filter[0][0]
+        seed_to_region.sort(key=lambda x: x[0])
+        lowest = seed_to_region[0][0]
 
     print(f'lowest {lowest}')
 
