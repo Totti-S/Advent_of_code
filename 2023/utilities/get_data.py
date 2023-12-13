11d317a9fb08af66daa2ded91294d0b764994fcc
Totti Sillanpää <totti.sillanpaa@gmail.com>
2023-12-13 08:16:28 +0200

    Updated utility function

--- 2023/utilities/get_data.py
+++ 2023/utilities/get_data.py
@@ -1,5 +1,5 @@
 import os
-def get_data(filepath, data_type='', line_is_numbers=False):
+def get_data(filepath, data_type='', line_is_numbers=False, has_portions=False):
 
     file = os.path.realpath(filepath)
     name = file.split('/')[-1].rstrip('.py')
@@ -8,8 +8,14 @@ def get_data(filepath, data_type='', line_is_numbers=False):
     path = f'data/{name}{data_type}_data.txt'
 
     with open(path, "r") as f:
-        data = f.read().splitlines()
-        if line_is_numbers:
-            data = [[int(x) for x in line.split()] for line in data]
+        if has_portions:
+            data = f.read().split('\n\n')
+            data = [d.split('\n') for d in data]
+            if line_is_numbers:
+                [[[int(x) for x in line.split()] for line in portion] for portion in data]
+        else:
+            data = f.read().splitlines()
+            if line_is_numbers:
+                data = [[int(x) for x in line.split()] for line in data]
 
     return data
