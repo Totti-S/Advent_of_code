e1be0b89d32cea5576dd43f13f48ae8411b252bd
Totti Sillanpää <totti.sillanpaa@gmail.com>
2023-12-14 16:38:48 +0200

    Cleaned solutions somewhat

--- 2023/day14.py
+++ 2023/day14.py
@@ -30,14 +30,8 @@ def main(mode='silver', data_type=''):
             new_columns.append(tmp)
         return new_columns
 
-    def transformation(array):  # 90 degrees to clockwise rotation
+    def rotate(array):  # 90 degrees to clockwise rotation
         return [[array[i][j] for i in range(len(data[0]))] for j in range(len(data)-1,-1,-1)]
-
-    def print_columns(columns):
-        by_rows = [[columns[i][j] for i in range(len(data))] for j in range(len(data[0]))]
-        by_rows = [''.join(line) for line in by_rows]
-        print('\n'.join(by_rows))
-        print()
 
     by_columns = [[data[i][j] for i in range(len(data))] for j in range(len(data[0]))]
     by_strings = tilt(by_columns)
@@ -55,57 +49,22 @@ def main(mode='silver', data_type=''):
     columns = [[data[i][j] for i in range(len(data))] for j in range(len(data[0]))]
     tmp = []
     cycles = 1_000_000_000
-    s = perf_counter()
-    found = False
-    index = 0
     tmp.append(columns.copy())
     for i in range(cycles):
-        for j in range(4):
-            # if i < 2:
-            #     print(i, j)
-            #     print('columns')
-            #     print_columns(columns)
+        for _ in range(4):
             tilted = tilt(columns)
-            columns = transformation(tilted)
-            # if i < 2:
-            #     print('tilted')
-            #     print_columns(tilted)
-        if not found and columns in tmp and i < 100:
-            found = True
+            columns = rotate(tilted)
+        if columns in tmp and i < 100:
             print('found cycle', i)
-            index = i
             permutations = tmp[tmp.index(columns):]
             break
-        elif not found:
-            tmp.append(columns.copy())
-        # elif found and columns in tmp:
-        #     print('Found cycle', i, tmp.index(columns))
+        tmp.append(columns.copy())
     
-    cycles_left = cycles - index - 1
+    cycles_left = cycles - i - 1
+    cycle_end_index = cycles_left % len(permutations)
+    last_position = permutations[cycle_end_index]
 
-    correct_index = cycles_left % len(permutations)
-    position = permutations[correct_index]
-
-
-
-
-
-    # # by_rows = [[north_tilted[i][j] for i in range(len(data))] for j in range(len(data[0]))]
-    # # by_rows = [''.join(line) for line in by_rows]
-    
-    # # print('\n'.join(by_rows))
-
-    # rotated = [[north_tilted[j][i] for i in range(len(data))] for j in range(len(data[0]))]
-    # # rotated = [list(reversed(line) for line in rotated]
-
-    # # by_rows = [[rotated[i][j] for i in range(len(data)-1,-1,-1)] for j in range(len(data[0]))]
-    # # by_rows = [''.join(line) for line in by_rows]
-    # # print('jälkeen:')
-    # # print('\n'.join(by_rows))
-    
-    # by_columns = [[data[i][j] for i in range(len(data))] for j in range(len(data[0]))]
-
-    by_rows = [[position[i][j] for i in range(len(data))] for j in range(len(data[0]))]
+    by_rows = [[last_position[i][j] for i in range(len(data))] for j in range(len(data[0]))]
     by_rows = [''.join(line) for line in by_rows]
 
     gold = 0
