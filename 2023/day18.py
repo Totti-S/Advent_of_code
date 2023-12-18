3372ac850b564eaf96bbf35d58002096ab4f0cdc
Totti Sillanpää <totti.sillanpaa@gmail.com>
2023-12-18 16:58:32 +0200

    Day 18 gold

--- 2023/day18.py
+++ 2023/day18.py
@@ -9,66 +9,95 @@ def main(mode='silver', data_type=''):
         'U' : (-1, 0),
         'D' : (1, 0)
     }
+    hexi = {
+        '0' : 'R',
+        '1' : 'D',
+        '2' : 'L',
+        '3' : 'U'
+    }
 
     current_node = (0,0)
     border = set([current_node])
-    for line in data:
-        direction, dig_out = re.match(r'(R|D|L|U) (\d+)', line).groups()
-        increment = direction_step[direction]
+    if mode == 'silver':
+        for line in data:
+            direction, dig_out, hexacode = re.match(r'(R|D|L|U) (\d+)', line).groups()
+
+            increment = direction_step[direction]
+            
+            for i in range(int(dig_out)):
+                current_node = (current_node[0] + increment[0], current_node[1] + increment[1])
+                border.add(current_node)
+
+        deepest_row = max([x[0] for x in border])
+        widest_col = max([x[1] for x in border])
+        col_offset = min([x[1] for x in border])
+        row_offset = min([x[0] for x in border])
+
+        all_stats = [['.' for j in range(widest_col+1-col_offset)] for i in range(-row_offset+ deepest_row+1)]
+        for node in border:
+            all_stats[node[0]-row_offset][node[1] -col_offset] = '#'
         
-        for i in range(int(dig_out)):
-            current_node = (current_node[0] + increment[0], current_node[1] + increment[1])
-            border.add(current_node)
+        all_stats = ["".join(line) for line in all_stats]
 
-    deepest_row = max([x[0] for x in border])
-    widest_col = max([x[1] for x in border])
-    col_offset = min([x[1] for x in border])
-    row_offset = min([x[0] for x in border])
+        with open('tmp.txt', 'w') as f:
+            for stat in all_stats:
+                f.write(stat + "\n")
 
-    all_stats = [['.' for j in range(widest_col+1-col_offset)] for i in range(-row_offset+ deepest_row+1)]
-    for node in border:
-        all_stats[node[0]-row_offset][node[1] -col_offset] = '#'
-    
-    all_stats = ["".join(line) for line in all_stats]
+        # Find a point that is for certain to be inside
+        first_dot = None
+        for row in range(row_offset, deepest_row+1):
+            tmp_list = list(filter(lambda x:x[0] == row, border))
+            tmp_cols = [x[1] for x in tmp_list]
+            tmp_cols.sort()
 
-    with open('tmp.txt', 'w') as f:
-        for stat in all_stats:
-            f.write(stat + "\n")
+            first = tmp_cols[0]
+            second = tmp_cols[1]
+            if abs(second - first) == 1:
+                continue
+            first_dot = (row, first+1)
+            break
 
-    # Find a point that is for certain to be inside
-    first_dot = None
-    for row in range(row_offset, deepest_row+1):
-        tmp_list = list(filter(lambda x:x[0] == row, border))
-        tmp_cols = [x[1] for x in tmp_list]
-        tmp_cols.sort()
+        assert first_dot is not None, 'Ei menny hyvin!'
+        
+        # Count by spreding to neigbouring dots
+        queue = [first_dot]
+        visited = set()
+        total = 0
+        while queue:
+            current_dot = queue.pop(0)
+            for direction in ['R', 'L', 'U', 'D']:
+                step = direction_step[direction]
+                next_dot = (current_dot[0] + step[0], current_dot[1] + step[1])
+                if next_dot not in border and next_dot not in visited:
+                    total += 1
+                    queue.append(next_dot)
+                    visited.add(next_dot)
+        total += len(border)
 
-        first = tmp_cols[0]
-        second = tmp_cols[1]
-        if second - first == 1:
-            continue
-        first_dot = (row, first+1)
-        break
+        print(f'{mode} : {total}')
 
-    assert first_dot is not None, 'Ei menny hyvin!'
-    
-    # Count by spreding to neigbouring dots
-    queue = [first_dot]
-    visited = set()
-    total = 0
-    while queue:
-        current_dot = queue.pop(0)
-        for direction in ['R', 'L', 'U', 'D']:
+    else: 
+        current_node = (0,0)
+        border = []
+        top_sum = 0
+        total_meters = 0
+        for line in data:
+            _,_, hexacode = re.match(r'(R|D|L|U) (\d+) \(#(.+)\)', line).groups()
+            meters = int(hexacode[0:5], base=16)
+            direction = hexi[hexacode[5]]
+            
             step = direction_step[direction]
-            next_dot = (current_dot[0] + step[0], current_dot[1] + step[1])
-            if next_dot not in border and next_dot not in visited:
-                total += 1
-                queue.append(next_dot)
-                visited.add(next_dot)
-    total += len(border)
+            next_node = (current_node[0] + step[0]*meters, current_node[1] + step[1]*meters)
 
-    print(f'{mode} : {total}')
+            total_meters += meters
+            top_sum += (current_node[0] * next_node[1] - current_node[1] * next_node[0])
+            current_node = next_node
+        
+        total = abs(top_sum) // 2
+        total += total_meters // 2
+        total += 1
+        print(total)
+
 
 if __name__ == "__main__":
-    print('test: ', end='')
-    main(data_type='test2')
-    main()
+    main('gold')
