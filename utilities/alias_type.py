8c6631535d5b59dd2cceaa98ef5b26f2338cd8ca
Totti Sillanpää <totti.sillanpaa@magister.fi>
2024-12-01 00:14:29 +0200

    Added enum class 'compass'

--- utilities/alias_type.py
+++ utilities/alias_type.py
@@ -1,3 +1,10 @@
 import typing as t
+from enum import Enum
 
 Mode = t.Literal["silver", "gold", "both"]
+
+class Compass(Enum):
+    North = (0, 1)
+    South = (0, -1)
+    East = (1, 0)
+    West = (0, -1)
