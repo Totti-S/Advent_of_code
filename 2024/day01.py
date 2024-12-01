92ffc1d4c51c9e310347911d5ca9ad162af8fea0
Totti Sillanpää <totti.sillanpaa@gmail.com>
2024-12-01 07:14:45 +0200

    Day01 both

--- 2024/day01.py
+++ 2024/day01.py
@@ -1,15 +1,35 @@
 from utilities.get_data import get_data
 from utilities.alias_type import Mode
 
+from collections import Counter
+
 def main(mode: Mode ='silver', data_type: str = ''):
     data = get_data(__file__, data_type, line_is_numbers=False)
 
     print(data)
+    st, nd = [], []
     for line in data:
-        pass
+        print(line)
+        j, k = line.split('   ')
+        
 
-    total = 0
+        st.append(int((j.strip())))
+        nd.append(int((k.strip())))
+
+    st.sort()
+    nd.sort()
+    if mode == 'silver':
+            
+        distances = []
+        for i, _ in enumerate(st):
+            distances.append(abs(st[i] - nd[i]))
+        total = sum(distances)
+
+    elif mode == 'gold':
+        occurances = Counter(nd)
+        multi = [num * occurances[num] for num in st]
+        total = sum(multi)
+
     print(f'{mode} : {total}')
-
 if __name__ == "__main__":
-    main(data_type='')
+    main(mode='gold', data_type='')
