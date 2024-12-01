b5a9c7ea5abf0510f0333111fadfcd11f793ad5f
Totti Sillanpää <totti.sillanpaa@gmail.com>
2024-12-01 22:41:47 +0200

    Fixed template making

--- utilities/make_template.py
+++ utilities/make_template.py
@@ -18,20 +18,17 @@ def main():
     :year(numbers only, opt.): Year number folder
 
     """
-    day = argv[1] if len(argv) > 1 else datetime.now().day
-    year = argv[2] if len(argv) > 2 else int(datetime.now().year)
+    day = int(argv[1]) if len(argv) > 1 else datetime.now().day
+    year = int(argv[2]) if len(argv) > 2 else int(datetime.now().year)
 
-    year = int(datetime.now().year)
-
-    if os.path.exists(f'../{year}/day{day}.py'):
+    day_str = str(day).zfill(2)
+    if os.path.exists(f'../{year}/day{day_str}.py'):
         print(f'Day {day} ".py"-file exits: making next day files')
-        day_str = str(int(day)+1).zfill(2)
-    else:
-        day_str = str(day).zfill(2)
+        day_str = str(day+1).zfill(2)
 
-    shutil.copy2("utilities/day00.py", f"{year}/day{day_str}.py")
+    shutil.copy2("../utilities/day00.py", f"../{year}/day{day_str}.py")
 
-    base_path = f'{year}/data/day{day_str}'
+    base_path = f'../{year}/data/day{day_str}'
     with open(f'{base_path}_data.txt', 'w'):
         pass
     with open(f'{base_path}_test_data.txt', 'w'):
