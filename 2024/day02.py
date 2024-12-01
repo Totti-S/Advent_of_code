912189a172469643c105b745a6cc2e3f21fd5f4c
Totti Sillanpää <totti.sillanpaa@gmail.com>
2024-12-01 22:42:16 +0200

    Templates for day 2

---
+++ 2024/day02.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append('..')
+from utilities.get_data import get_data
+from utilities.alias_type import Mode
+
+def main(mode: Mode ='silver', data_type: str = ''):
+    data = get_data(__file__, data_type, line_is_numbers=False)
+
+    print(data)
+    for line in data:
+        pass
+
+    total = 0
+    print(f'{mode} : {total}')
+
+if __name__ == "__main__":
+    main(data_type='test')
