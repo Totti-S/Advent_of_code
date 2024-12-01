61ea8599a6b25919ef8319eb4d2af83e735fae9b
Totti Sillanpää <totti.sillanpaa@gmail.com>
2024-12-01 21:47:00 +0200

    Fixed get_data pathing

--- utilities/get_data.py
+++ utilities/get_data.py
@@ -8,17 +8,20 @@ def get_data(
     has_portions: bool = False
 ):
     file = os.path.realpath(filepath)
-    name = file.split('/')[-1].rstrip('.py')
+    file_name_list = file.split('/')
+    name = file_name_list[-1].rstrip('.py')
+    year = file_name_list[-2]
     if data_type != '':
         data_type = '_' + data_type
-    path = f'data/{name}{data_type}_data.txt'
+    path = f'{year}/data/{name}{data_type}_data.txt'
 
     with open(path, "r") as f:
         if has_portions:
             data = f.read().split('\n\n')
             data = [d.split('\n') for d in data]
             if line_is_numbers:
-                [[[int(x) for x in line.split()] for line in portion] for portion in data]
+                data = [[re.findall(r'\d+', line) for line in portion] for portion in data]
+                data = [[list(map(int, line)) for line in portion] for portion in data]
         else:
             data = f.read().splitlines()
             if line_is_numbers:
