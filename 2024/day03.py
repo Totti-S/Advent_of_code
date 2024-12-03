976d422f7912dfb13a0dbab249ae4517342daf25
Totti Sillanpää <totti.sillanpaa@magister.fi>
2024-12-03 12:27:11 +0200

    Day 3 Gold

--- 2024/day03.py
+++ 2024/day03.py
@@ -8,21 +8,28 @@ import re
 def main(mode: Mode ='silver', data_type: str = ''):
     data = get_data(__file__, data_type, line_is_numbers=False)
 
-    print(data)
-    commands : list[str]
+    data: str = "".join(data)
     total = 0
-    for line in data:
-        commands = re.findall(r'mul\(\d+\,\d+\)', line)
+    commands: list[re.Match]
+    acceptable_commands: list[str] = []
 
-        for command in commands:
-            numbers = command.lstrip("mul(").rstrip(')').split(",")
-            numbers = list(map(int, numbers))
-            total += int(numbers[0]) * int(numbers[1])
+    commands = list(re.finditer(r'mul\(\d+\,\d+\)', data))
+    for command in commands:
+        start_idx = command.start()
+        behind_line = data[:start_idx]
+        behind_line = behind_line[::-1]
 
+        yes = behind_line.find(")(od")
+        no = behind_line.find(")(t'nod")
+        if no == -1 or (yes < no and yes != -1) or mode == "silver":
+            acceptable_commands.append(command.group())
 
-
+    for command in acceptable_commands:
+        numbers = command.lstrip("mul(").rstrip(')').split(",")
+        numbers = list(map(int, numbers))
+        total += int(numbers[0]) * int(numbers[1])
 
     print(f'{mode} : {total}')
 
 if __name__ == "__main__":
-    main(data_type='')
+    main(mode="gold", data_type='')
