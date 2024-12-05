2fdc90b87ff9b518c5bdb538cc34edfb69922c90
Totti Sillanpää <totti.sillanpaa@gmail.com>
2024-12-05 16:00:03 +0200

    Added helpper functions "is_sublist"

--- utilities/helper_funs.py
+++ utilities/helper_funs.py
@@ -1,3 +1,7 @@
+import typing as t
 
 def nums(a: list[str]) -> list[int]:
     return list(map(int, a))
+
+def is_sublist(a: list[t.Any], b: list[t.Any]) -> bool:
+    return all([item in b for item in a])
